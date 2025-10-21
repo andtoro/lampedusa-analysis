d44c2a9161d9727332253be77eb548c377e37111
andtoro <andrea@andreAT>
2025-10-21 16:42:33 +0200

    configuration file

---
+++ config.py
@@ -0,0 +1,11 @@
+# Configuration file with all settings
+NETCDF_FILE = 'data/your_data.nc'
+REFLECTANCE_VAR = 'reflectance'
+TIME_DIM = 'time'
+LAT_DIM = 'lat'
+LON_DIM = 'lon'
+WAVELENGTH_DIM = 'wavelength'
+
+OUTPUT_DIR = 'outputs/'
+FIGURE_DIR = 'outputs/figures/'
+CSV_DIR = 'outputs/csv/'
